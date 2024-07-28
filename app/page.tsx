efd368673141acc2e96b4f964a7d5c0f66c71afa
Josh Worden <2015JTW@gmail.com>
2024-07-27 21:18:11 -0400

    repo created
    start of project

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex max-w-2xl w-full justify-center h-52 items-center border border-black">
+        <h1 className="text-2xl font-semibold">Josh Portfolio</h1>
+      </div>
+    </main>
+  );
+}
