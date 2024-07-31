808905ade838d6d86228dd9b476431b6ee605d99
Josh Worden <2015JTW@gmail.com>
2024-07-31 17:47:19 -0400

    vercel fix

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -39,7 +39,7 @@ const Footer = () => {
               key={item.id}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
-              <img src={item.img} alt={item.id} />
+              <img src={item.img} alt={`${item.id}`} />
             </div>
           ))}
         </div>
