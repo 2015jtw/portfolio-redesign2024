f9c36dc4fc12174576b1662d9fdbec0bba15aea8
Josh Worden <2015JTW@gmail.com>
2024-07-30 21:41:02 -0400

    built my approach section

--- app/components/RecentProjects.tsx
+++ app/components/RecentProjects.tsx
@@ -6,7 +6,7 @@ import { FaLocationArrow } from "react-icons/fa";
 
 const RecentProjects = () => {
   return (
-    <div className="pb-96 py-20" id="projects">
+    <div className="py-20" id="projects">
       <h1 className="heading">
         A Small Selection of{" "}
         <span className="text-purple">Recent Projects</span>
