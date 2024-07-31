a10879cb5054ab1634fb7fca80608e7d7adaed28
Josh Worden <2015JTW@gmail.com>
2024-07-31 14:20:16 -0400

    starting work experience

--- app/components/WorkExperience.tsx
+++ app/components/WorkExperience.tsx
@@ -5,13 +5,20 @@ import {
 } from "react-vertical-timeline-component";
 import { motion } from "framer-motion";
 import "react-vertical-timeline-component/style.min.css";
+import { styles } from "../styles";
+import { workExperience } from "@/data";
+import { SectionWrapper } from "../hoc";
+import { textVariant } from "../../lib/motion";
 
 const WorkExperience = () => {
   return (
-    <div className="w-full flex justify-center">
-      <h1 className="text-3xl text-white">Work Experience</h1>
-    </div>
+    <>
+      <motion.div variants={textVariant(0.5)}>
+        <p className={styles.sectionSubText}>What I have done so far</p>
+        <p className={styles.sectionHeadText}>Work Experience</p>
+      </motion.div>
+    </>
   );
 };
 
-export default WorkExperience;
+export default SectionWrapper(WorkExperience, "work-experience");
