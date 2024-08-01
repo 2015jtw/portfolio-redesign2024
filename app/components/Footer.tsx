ab87ff63cac5ac33cc606b61f7dc19b562f2cb07
Josh Worden <2015JTW@gmail.com>
2024-07-31 21:44:48 -0400

    improve footer layout

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ import { socialMedia } from "@/data";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
+    <footer className="w-full py-10" id="contact">
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
         <img
           src="/footer-grid.svg"
@@ -29,7 +29,7 @@ const Footer = () => {
           />
         </a>
       </div>
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+      <div className="flex md:flex-row flex-col justify-between items-center mt-20">
         <p className="md:text-base text-sm md:font-normal font-light">
           Copyright © 2024 JTW. All rights reserved.
         </p>
