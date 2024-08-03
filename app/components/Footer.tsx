94760f5f324b57d51e29fd82c098df127a81985d
Josh Worden <2015JTW@gmail.com>
2024-08-03 13:33:37 -0400

    complete light mode

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,24 +1,33 @@
+"use client";
 import React from "react";
 import MagicButton from "./ui/magicButton";
 import { FaLocationArrow } from "react-icons/fa";
 import { socialMedia } from "@/data";
+import { useTheme } from "next-themes";
 
 const Footer = () => {
+  const { theme } = useTheme();
+
   return (
     <footer className="w-full py-10" id="contact">
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
         <img
           src="/footer-grid.svg"
           alt="footer-grid"
-          className="w-full h-full opacity-40"
+          className={`w-full h-full opacity-40 ${
+            theme === "light" && "filter-light-mode"
+          }`}
         />
       </div>
       <div className="flex flex-col items-center ">
         <h1 className="heading lg:max-w-[45vw]">
-          Ready to take <span className="text-purple">your next project</span>{" "}
+          Ready to take{" "}
+          <span className="text-blue-500 dark:text-purple">
+            your next project
+          </span>{" "}
           to the next level?
         </h1>
-        <p className="text-white-200 md:mt-10 my-5 text-center">
+        <p className="text-black dark:text-white md:mt-10 my-5 text-center">
           Reach out to me and let&apos;s discuss your project.
         </p>
         <a href="mailto:2015JTW@gmail.com">
