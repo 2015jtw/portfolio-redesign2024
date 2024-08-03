752eb53ecee8f0310613c288b19f2f2e89b21dfa
Josh Worden <2015JTW@gmail.com>
2024-08-03 14:06:37 -0400

    more errors

--- app/components/ui/bentoGrid.tsx
+++ app/components/ui/bentoGrid.tsx
@@ -120,7 +120,7 @@ export const BentoGridItem = ({
             {description}
           </div>
           <div
-            className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10`}
+            className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10 pb-4 md:pb-0`}
           >
             {title}
           </div>
