1425775a492226d9dc026ee6bce3ac35758ceae4
Josh <2015jtw@gmail.com>
2025-01-08 12:27:35 +0100

    optimizing build performance

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
-
+  reactStrictMode: false,
 };
 
 export default nextConfig;
