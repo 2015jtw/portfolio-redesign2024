4b47dc0e9fe41d616d0f9691ef623e28a4191219
Josh <2015jtw@gmail.com>
2025-01-08 12:38:06 +0100

    fix config

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 import withBundleAnalyzer from "@next/bundle-analyzer";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  experimental: {
-    turbo: true,
-  },
-};
+const nextConfig = {};
 
 export default withBundleAnalyzer({
   enabled: process.env.ANALYZE === "true",
