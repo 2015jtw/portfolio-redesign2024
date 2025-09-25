ec56af08f74d2cb7722f4b3b0eb8ec49d933d8a6
joshua <joshua@magemontreal.com>
2025-09-26 01:41:34 +0500

    sanity fix

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,16 +1,16 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-09-25'
 
-// use NEXT_PUBLIC_SANITY_STUDIO_DATASET for local development and SANITY_STUDIO_DATASET for production
+// use NEXT_PUBLIC_SANITY_DATASET for local development and SANITY_STUDIO_DATASET for production
 export const dataset = assertValue(
-  process.env.SANITY_STUDIO_DATASET,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_STUDIO_DATASET"
+  process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_STUDIO_DATASET,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET or SANITY_STUDIO_DATASET"
 );
 
-// use NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID for local development and SANITY_STUDIO_PROJECT_ID for production
+// use NEXT_PUBLIC_SANITY_PROJECT_ID for local development and SANITY_STUDIO_PROJECT_ID for production
 export const projectId = assertValue(
-  process.env.SANITY_STUDIO_PROJECT_ID,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID"
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.SANITY_STUDIO_PROJECT_ID,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID or SANITY_STUDIO_PROJECT_ID"
 );
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
